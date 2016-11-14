c460424988d99a2d6c95c398bdafcb1efd6bc42e
Shubham Sakhuja <ssakhuja@Shubhams-MacBook-Pro.local>
2016-11-14 10:17:12 -0500

    adding RestHelper into main file

--- lib/testify.py
+++ lib/testify.py
@@ -1,14 +1,18 @@
-from RestHelper import RestHelper
+#!/usr/bin/python
+
 import json
 import os
 import time
 import datetime
-from jsonconv import *
+#from jsonconv import *
 import re
 import copy
 import abc
 import traceback
 from subprocess import Popen, PIPE
+import ssl
+import httplib
+
 
 # constants
 DEBUG_LEVEL = 0
@@ -73,6 +77,36 @@ def get_task(task_type):
         if t.__name__ == task_type:
             return t
     return None
+
+class RestHelper(object):
+
+    def __init__(self, host, port, secure, check_cert=False, auth=None):
+        self.host = host
+        self.port = port
+        self.secure = secure
+    self.check_cert = check_cert
+    self.auth = auth
+
+    def set_auth(self, auth):
+    self.auth = auth
+
+    def request(self, method, uri, data=None, content_type='application/json', headers={}, auth='default', timeout=60):
+        if self.secure:
+        if self.check_cert:
+                    conn = httplib.HTTPSConnection(self.host, self.port, timeout=timeout)
+        else:
+            conn = httplib.HTTPSConnection(self.host, self.port, context=ssl._create_unverified_context(), timeout=timeout)
+        else:
+        conn = httplib.HTTPConnection(self.host, self.port, timeout=timeout)
+
+    auth = self.auth if auth == 'default' else auth
+
+    headers['Authorization'] = auth
+
+    headers['Content-type'] = content_type
+
+        conn.request(method, uri, data, headers=headers)
+        return conn.getresponse()
 
 # task
 
